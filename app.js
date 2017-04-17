b59fc4bbbedbf4a72b21576905a59ba41cd6e0fb
Anthony Langford <anthony.langford@gmail.com>
2017-04-17 13:58:05 -0400

    :arrow_up:

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express    = require('express');
+const request    = require('request');
+const bodyParser = require("body-parser");
 const app        = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.listen(8080, '0.0.0.0', () => {
     console.log('Listening to port: 8080');
