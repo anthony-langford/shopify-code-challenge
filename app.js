a24a40191b62b4b567f87ec92ae761cb6f57d32f
Anthony Langford <anthony.langford@gmail.com>
2017-04-21 13:08:51 -0400

    moved sorting functions into app.js. need promises?

--- app.js
+++ app.js
@@ -5,56 +5,139 @@ const app     = express();
 
 let url = 'https://backend-challenge-fall-2017.herokuapp.com/orders.json';
 let totalPages = null;
-let currentPage = 1;
+let currentPage = 0;
 let availableCookies = 0;
 let orders = [];
 let unfulfilledOrders = [];
-let ordersToBeFulfilled = [];
+let pendingOrders = [];
+
+
+// Find object in array with given key and return index
+Array.prototype.find = function(obj) {
+  // Loop through array
+  for (let i = 0, len = this.length; i < len; i++) {
+    let ele = this[i];
+    let match = true;
+    // Check each object
+    for (let x in obj) {
+      if (ele[x] !== obj[x]) {
+        match = false;
+        break;
+      }
+    }
+    // Did it match?
+    if (match) {
+      return i;
+    }
+  }
+};
+
+// Find any order with given key/value pair
+function findWithAttr(array, key, value) {
+  for (let i = 0; i < array.length; i++) {
+    if(array[i][key] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+
+
 
 
 // Filters out fulfilled orders
-function removeFulfilledOrders(array) {
-  unfulfilledOrders = array.filter((order) => {
+function removeFulfilledOrders() {
+  unfulfilledOrders = orders.filter((order) => {
     return order.fulfilled !== true;
   })
-  console.log('unfulfilled orders', unfulfilledOrders);
 }
+
+// removeFulfilledOrders();
+// console.log(unfulfilledOrders);
+
+
+
+// Push all orders without cookies first
+function pushOrdersWithoutCookies() {
+  let sortingArray = [];
+  for (let i = 0; i < unfulfilledOrders.length; i++) {
+    if (findWithAttr(unfulfilledOrders[i].products, 'title', 'Cookie') === -1) {
+        pendingOrders.push(unfulfilledOrders[i]);
+        unfulfilledOrders.splice(i, 1);
+    };
+  }
+}
+
+// pushOrdersWithoutCookies();
+// console.log('unfulfilledOrders', unfulfilledOrders);
+// console.log('pendingOrders', pendingOrders);
+
+
+
+function sortUnfulfilledOrdersByCookies() {
+  unfulfilledOrders.sort((a, b) => {
+    return b.products[b.products.find({ title: 'Cookie' })].amount - a.products[a.products.find({ title: 'Cookie' })].amount;
+  })
+}
+
+// sortUnfulfilledOrdersByCookies();
+// console.log('unfulfilledOrders', unfulfilledOrders);
+
+
+
+function pushCookieOrders() {
+  let indexesToRemove = [];
+  for (let i = 0; i < unfulfilledOrders.length; i++) {
+    let orderCookieCount = unfulfilledOrders[i].products[unfulfilledOrders[i].products.find({ title: 'Cookie' })].amount;
+    if (orderCookieCount <= remainingCookies) {
+      remainingCookies -= orderCookieCount;
+      pendingOrders.push(unfulfilledOrders[i]);
+      indexesToRemove.push(i);
+    }
+  }
+  for (let i = indexesToRemove.length - 1; i >= 0; i--) { // Need to loop in reverse order to remove orders so index doesn't change every cycle
+    unfulfilledOrders.splice(indexesToRemove[i], 1);
+  }
+}
+
+// pushCookieOrders();
+// console.log('unfulfilledOrders', unfulfilledOrders);
+// console.log('pendingOrders', pendingOrders);
+
 
 
 // Gets data from all pages
 function getAllPaginatedData(uri, callback) {
 
+  request({ uri: uri, json: true }, (error, response, body) => {
 
-    request({ uri: uri, json: true }, (error, response, body) => {
+    console.log('error:', error); // Log the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Log the response status code if a response was received
+    console.log('body:', body); // Log the body
 
+    if (!error && response.statusCode === 200) {
 
-      console.log('error:', error); // Log the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Log the response status code if a response was received
-      console.log('body:', body); // Log the body
+      currentPage = body.pagination.current_page;
 
-
-      if (!error && response.statusCode === 200) {
-
-        currentPage = body.pagination.current_page;
-
-        if (currentPage === 1) { // Set total pages and available cookies on first page
-          totalPages = body.pagination.total;
-          availableCookies = body.available_cookies;
-        }
-
-        console.log('currentPage', currentPage);
-        console.log('totalPages', totalPages);
-
-        if (body.orders.length > 0) {  // Save orders if any on page
-          orders = orders.concat(body.orders);
-        }
-
-      } else {
-        new Error('Error: ', error);
+      if (currentPage === 1) { // Set total pages and available cookies on first page
+        totalPages = body.pagination.total;
+        availableCookies = body.available_cookies;
       }
 
+      console.log('currentPage', currentPage);
+      console.log('totalPages', totalPages);
+
+      if (body.orders.length > 0) {  // Save orders if any on page
+        orders = orders.concat(body.orders);
+      }
+
+    } else {
+      new Error('Error: ', error);
+    }
+
     if (currentPage === totalPages) { // Callback if last page
-      removeFulfilledOrders(orders);
       callback;
     } else {
       currentPage ++;
@@ -62,8 +145,6 @@ function getAllPaginatedData(uri, callback) {
       getAllPaginatedData(`${url}?page=${currentPage.toString()}`);
     }
   })
-
-
 }
 
 
@@ -71,6 +152,12 @@ function getAllPaginatedData(uri, callback) {
 
 app.get('/', (req, res) => {
   getAllPaginatedData(url, () => {
+    // removeFulfilledOrders();
+    // pushOrdersWithoutCookies();
+    // sortUnfulfilledOrdersByCookies();
+    // pushCookieOrders();
+    // console.log('unfulfilledOrders', unfulfilledOrders);
+    // console.log('pendingOrders', pendingOrders);
     console.log('Done!');
   });
   res.status(200).send('Hello!');
