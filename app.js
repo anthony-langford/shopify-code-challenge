8bda86884e95d303658f41ebb04dc8636a074ae9
Anthony Langford <anthony.langford@gmail.com>
2017-04-23 14:36:37 -0400

    promises fixed. added Promise.all([all async requests]) to solve async issue. need to refactor functions to be more independent and reusable, global vars, and add promises to make sure async functions will work with larger datasets.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app     = express();
 let url = 'https://backend-challenge-fall-2017.herokuapp.com/orders.json';
 let totalPages = null;
 let currentPage = 0;
-let availableCookies = 0;
+let remainingCookies = 0;
 let orders = [];
 let unfulfilledOrders = [];
 let pendingOrders = [];
@@ -44,50 +44,79 @@ function findWithAttr(array, key, value) {
 
 
 
+function getPages(uri) {
+  totalPages = null;
+  currentPage = 0;
+  remainingCookies = 0;
+  orders = [];
+  unfulfilledOrders = [];
+  pendingOrders = [];
+  return new Promise((resolve, reject) => {
+    request({ uri: uri, json: true }, (err, res, body) => {
 
-// Gets data from all pages
-function getAllPaginatedData(uri, callback) {
+      console.log('error:', err); // Log the error if one occurred
+      console.log('statusCode:', res && res.statusCode); // Log the response status code if a response was received
+      console.log('body:', body); // Log the body
 
-  request({ uri: uri, json: true }, (error, response, body) => {
-
-    console.log('error:', error); // Log the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Log the response status code if a response was received
-    console.log('body:', body); // Log the body
-
-    if (!error && response.statusCode === 200) {
-
-      currentPage = body.pagination.current_page;
-
-      if (currentPage === 1) { // Set total pages and available cookies on first page
+      if (!err && res.statusCode === 200) {
         totalPages = body.pagination.total;
-        availableCookies = body.available_cookies;
+        remainingCookies = body.available_cookies;
+        console.log('totalPages', totalPages);
+        console.log('remainingCookies', remainingCookies);
+        resolve();
+      } else {
+        reject(Error(err));
       }
 
-      console.log('currentPage', currentPage);
-      console.log('totalPages', totalPages);
+    });
+  });
+}
 
-      if (body.orders.length > 0) {  // Save orders if any on page
-        orders = orders.concat(body.orders);
+
+
+
+function getPageData(uri) {
+  return new Promise((resolve, reject) => {
+    request({ uri: uri, json: true }, (err, res, body) => {
+      console.log('PAGE', body.pagination.current_page);
+      console.log('error:', err); // Log the error if one occurred
+      console.log('statusCode:', res && res.statusCode); // Log the response status code if a response was received
+      console.log('body:', body); // Log the body
+
+      if (!err && res.statusCode === 200) {
+        if (body.orders.length > 0) {  // Save orders if any on page
+          orders = orders.concat(body.orders);
+        }
+        resolve();
+      } else {
+        reject(Error(err));
       }
-
-    } else {
-      new Error('Error: ', error);
-    }
-
-    if (currentPage === totalPages) { // Callback if last page
-      callback;
-    } else {
-      currentPage ++;
-      console.log('url', `${url}?page=${currentPage.toString()}`);
-      getAllPaginatedData(`${url}?page=${currentPage.toString()}`);
-    }
+    })
   })
 }
 
 
 
+
+function preparePromises(url) {
+  let promises = [];
+  return new Promise((resolve, reject) => {
+    for (i = 1; i <= totalPages; i++) {
+      let p = getPageData(`${url}?page=${i.toString()}`);
+      promises.push(p);
+    }
+    console.log('promises array', promises);
+    resolve(promises);                                                 // How to trigger this after all responses?¿?¿ˀˀˀˀˀ Promise.all([])?
+    // Need to run a single Promise.all([]) with every getPageData() inside array
+  })
+}
+
+
+
+
 // Filters out fulfilled orders
 function removeFulfilledOrders() {
+  console.log('Removing fulfilled orders');
   unfulfilledOrders = orders.filter((order) => {
     return order.fulfilled !== true;
   })
@@ -98,8 +127,10 @@ function removeFulfilledOrders() {
 
 
 
+
 // Push all orders without cookies first
 function pushOrdersWithoutCookies() {
+  console.log('Pushing orders without cookies');
   let sortingArray = [];
   for (let i = 0; i < unfulfilledOrders.length; i++) {
     if (findWithAttr(unfulfilledOrders[i].products, 'title', 'Cookie') === -1) {
@@ -115,7 +146,9 @@ function pushOrdersWithoutCookies() {
 
 
 
+
 function sortUnfulfilledOrdersByCookies() {
+  console.log('Sorting unfulfilled orders by cookies');
   unfulfilledOrders.sort((a, b) => {
     return b.products[b.products.find({ title: 'Cookie' })].amount - a.products[a.products.find({ title: 'Cookie' })].amount;
   })
@@ -126,7 +159,9 @@ function sortUnfulfilledOrdersByCookies() {
 
 
 
+
 function pushCookieOrders() {
+  console.log('Pushing orders with cookies');
   let indexesToRemove = [];
   for (let i = 0; i < unfulfilledOrders.length; i++) {
     let orderCookieCount = unfulfilledOrders[i].products[unfulfilledOrders[i].products.find({ title: 'Cookie' })].amount;
@@ -147,32 +182,73 @@ function pushCookieOrders() {
 
 
 
-function sortById() {
-  unfulfilledOrders.sort((a, b) => {
+
+function sortById(arr) {
+  console.log('Sorting by ID');
+  arr.sort((a, b) => {
     return a.id - b.id;
   })
 }
 
-// sortById();
+// sortById(unfulfilledOrders);
 // console.log('unfulfilledOrders', unfulfilledOrders);
 
 
 
+
+function stripToIds(arr) {
+  let unfulfilled_orders = [];
+  for (i = 0; i < unfulfilledOrders; i++) {
+    unfulfilled_orders.push(unfulfilledOrders.id)
+  }
+}
+
+
+
 app.get('/', (req, res) => {
-  getAllPaginatedData(url, () => {
-    // removeFulfilledOrders();
-    // pushOrdersWithoutCookies();
-    // sortUnfulfilledOrdersByCookies();
-    // pushCookieOrders();
-    // console.log('unfulfilledOrders', unfulfilledOrders);
-    // console.log('pendingOrders', pendingOrders);
-    console.log('Done!');
-  });
-  res.status(200).send('Hello!');
+  // Promise.all([getPages(url), getAllData(url)])
+  // .then(() => {
+  //   console.log('ORDERS', orders);
+  //   res.json({
+  //     'remaining_cookies': remainingCookies,
+  //     'unfulfilled_orders': unfulfilledOrders
+  //   })
+  // })
+  // .catch((err) => {
+  //   new Error(err);
+  // })
+
+  getPages(url)
+  .then(() => {
+    preparePromises(url)
+    .then((promises) => {
+      Promise.all(promises)
+      .then(() => {
+        console.log('Sorting orders', orders);
+        sortById(orders);
+        console.log('Sorted orders', orders);
+        removeFulfilledOrders();
+        pushOrdersWithoutCookies();
+        sortUnfulfilledOrdersByCookies();
+        pushCookieOrders();
+        sortById(unfulfilledOrders);
+        let unfulfilled_orders = [];
+        for (i = 0; i < unfulfilledOrders; i++) {
+          unfulfilled_orders.push(unfulfilledOrders.id)
+        }
+        res.json({
+          'remaining_cookies': remainingCookies,
+          'unfulfilled_orders': unfulfilledOrders
+        })
+      })
+    })
+  })
+
+
 });
 
 
 
-app.listen(8080, '0.0.0.0', () => {
-    console.log('Listening to port: 8080');
+app.listen(4444, '0.0.0.0', () => {
+    console.log('Listening to port: 4444');
 });
